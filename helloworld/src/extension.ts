a5e192a6acd734d16488132533b003a37bc5abaf
박준수 <tony180322@gmail.com>
2021-05-03 00:39:33 +0900

    feat: practice for using WebView API and make click button for creating react app

--- helloworld/src/extension.ts
+++ helloworld/src/extension.ts
@@ -13,14 +13,39 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HelloWorld!');
-	});
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World!');
+	// });
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('helloworld.helloWorld', () => {
+			const panel = vscode.window.createWebviewPanel(
+				'catCoding',
+				'Cat Coding',
+				vscode.ViewColumn.One,
+				{}
+			);
+
+			panel.webview.html = getWebviewContent();
+		})
+	);
+}
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <button type = "button">Click!</button>
+</body>
+</html>`;
 }
 
 // this method is called when your extension is deactivated
